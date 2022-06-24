d0d0636bafa3e4c7a10c24aea86a15c359f6224b
Kawu <lucasrolla08@gmail.com>
2022-06-24 02:01:49 -0300

    Organizando entorno en SASS

--- typescript/index.ts
+++
@@ -1,8 +0,0 @@
-/* Cuando hago CLICK .button, .nav TOGGLE 'activo' */
-
-const button = (document.querySelector('.button') as HTMLButtonElement)
-const nav = (document.querySelector('.nav') as HTMLElement)
-
-button.addEventListener('click', () : void => {
-    nav.classList.toggle('activo')
-})
